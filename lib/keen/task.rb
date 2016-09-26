278153e1ea680e0fa9b651e88e6398335b67c17b
Lukas Angerer <lukas.angerer@getunik.com>
2016-09-26 08:59:50 +0200

    More method metadata experimenting

--- lib/keen/task.rb
+++ lib/keen/task.rb
@@ -2,5 +2,16 @@
 class Task
 	def self.command(metadata)
 		p(metadata)
+		@last = metadata
+	end
+
+	def self.method_added(m)
+		puts("defining method #{m}")
+		p(@last)
+
+		meth = self.instance_method(m)
+		p(meth)
+
+		@last = nil
 	end
 end
